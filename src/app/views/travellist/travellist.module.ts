73ad0c1e747aec7123e58b82dcd36796ca3aee76
Johncua <cuatumax@gmail.com>
2020-02-19 22:50:22 -0600

    carrucel aplicado, solo hay que mejorar su css

--- src/app/views/travellist/travellist.module.ts
+++ src/app/views/travellist/travellist.module.ts
@@ -5,11 +5,13 @@ import { CommonModule } from '@angular/common';
 import { TravellistRoutingModule } from './travellist-routing.module';
 import { TravellistIndexComponent } from './travellist-index/travellist-index.component';
 
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 @NgModule({
   declarations: [TravellistIndexComponent],
   imports: [
     CommonModule,
-    TravellistRoutingModule
+    TravellistRoutingModule,
+    CarouselModule
   ]
 })
 export class TravellistModule { }
